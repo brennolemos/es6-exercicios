4acd9989d9255238943f101014a45d23063d4bcd
Breno Lemos <brenomiros@gmail.com>
2019-02-04 13:51:34 -0300

    Exercício Variáveis

--- js-para-iniciantes/01-variaveis/script.js
+++ js-para-iniciantes/01-variaveis/script.js
@@ -1,29 +1,29 @@
-var nome = 'Breno';
-var idade = 23;
+// var nome = 'Breno';
+// var idade = 23;
 
-var possuiFaculdade = true;
+// var possuiFaculdade = true;
 
-console.log(nome, idade, possuiFaculdade);
+// console.log(nome, idade, possuiFaculdade);
 
-var preco = 25;
-var totalComprado = 5;
-var totalPreco = totalComprado * preco;
+// var preco = 25;
+// var totalComprado = 5;
+// var totalPreco = totalComprado * preco;
 
-console.log(totalPreco);
+// console.log(totalPreco);
 
-var sobrenome = 'Lemos',
-    cidade = 'Fortaleza';
+// var sobrenome = 'Lemos',
+//     cidade = 'Fortaleza';
 
-console.log(sobrenome, cidade);
+// console.log(sobrenome, cidade);
 
-var semDefinir;
-console.log(semDefinir);
+// var semDefinir;
+// console.log(semDefinir);
 
-// console.log(aindaNaoDefini);
-var comida;
-console.log(comida);
-comida = 'Pizza';
-console.log(comida);
+// // console.log(aindaNaoDefini);
+// var comida;
+// console.log(comida);
+// comida = 'Pizza';
+// console.log(comida);
 
 // const time = 'Vasco';
 // time = 'Flamengo';
@@ -32,3 +32,22 @@ console.log(comida);
 
 
 // Exercícios
+// Declarar uma variável com o seu nome
+var nome = 'Breno';
+
+// Declarar uma variável com a sua idade
+var idade = 23;
+
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+var comidaFavorita;
+
+// Atribuir valor a sua comida favorita
+comidaFavorita = 'Pizza';
+
+// Declarar 5 variáveis diferentes sem valores
+var valor1,
+    valor2,
+    valor3,
+    valor4,
+    valor5;
