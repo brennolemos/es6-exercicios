c9705671f888552963ade54ce2b677d99aff33d9
Breno Lemos <brenomiros@gmail.com>
2019-02-04 13:42:42 -0300

    Conteúdo de Variáveis

---
+++ js-para-iniciantes/01-variaveis/script.js
@@ -0,0 +1,34 @@
+var nome = 'Breno';
+var idade = 23;
+
+var possuiFaculdade = true;
+
+console.log(nome, idade, possuiFaculdade);
+
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = totalComprado * preco;
+
+console.log(totalPreco);
+
+var sobrenome = 'Lemos',
+    cidade = 'Fortaleza';
+
+console.log(sobrenome, cidade);
+
+var semDefinir;
+console.log(semDefinir);
+
+// console.log(aindaNaoDefini);
+var comida;
+console.log(comida);
+comida = 'Pizza';
+console.log(comida);
+
+// const time = 'Vasco';
+// time = 'Flamengo';
+
+// console.log(time);
+
+
+// Exercícios
