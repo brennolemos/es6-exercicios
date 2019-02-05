9b0317d9eee4bba234e29b96d6c24f2820c6d39f
Breno Lemos <brenomiros@gmail.com>
2019-02-04 22:19:34 -0200

    Exercícios da aula de Tipos de Dados

---
+++ js-para-iniciantes/02-tipos-de-dados/script.js
@@ -0,0 +1,20 @@
+// Declare uma variável contendo uma string
+var nome = 'Breno';
+
+// Declare uma variável contendo um número dentro de uma string
+var idade = '23';
+
+// Declare uma variável com a sua idade
+var idadeNumber = 23;
+
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+var nome = 'Breno',
+    sobrenome = 'Lemos';
+console.log(nome + sobrenome);
+
+// Coloque a seguinte frase em uma variável: It's time
+var frase = "It's time";
+
+// Verifique o tipo da variável que contém o seu nome
+console.log(typeof nome);
