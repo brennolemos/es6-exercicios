b20c1ad4ec7ce8b002fbe22f5362bd48d7cd10a9
Breno Lemos <brenomiros@gmail.com>
2019-02-06 13:13:11 -0300

    Script Números e Operadores 2

--- js-para-iniciantes/03-numeros-e-operadores/script-aula.js
+++ js-para-iniciantes/03-numeros-e-operadores/script-aula.js
@@ -5,3 +5,13 @@ console.log(modulo);
 
 var testeNaN = 'Isso é 100' / 2;
 console.log(isNaN(testeNaN));
+
+var soma1 = 10 + 10 + 20 + 30 * 4 / 2 + 10;
+console.log(soma1);
+
+var incremento = 5;
+console.log(++incremento);
+
+var idade = '28';
+var somaIdade = 5;
+console.log(+idade + somaIdade);
