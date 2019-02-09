cf5cc7f26ef9d6a4d5d5bfef32144f242170fbf6
Breno Lemos <brenomiros@gmail.com>
2019-02-08 22:50:00 -0200

    Script Boolean e Condicionais 2

--- js-para-iniciantes/04-booleans-e-condicionais/script-aula.js
+++ js-para-iniciantes/04-booleans-e-condicionais/script-aula.js
@@ -21,3 +21,33 @@ if (nome) {
 if (!possuiGraduacao) {
     console.log('Não possui graduação!')
 }
+
+if ((5 - 5) && (5 + 5)) {
+    console.log('verdadeiro');
+} else {
+    console.log('falso');
+}
+
+var condicional = (5 - 10) && (5 + 5);
+if (condicional) {
+    console.log('verdadeiro', condicional);
+} else {
+    console.log('falso');
+}
+
+var condicional2 = (5 - 5) || (5 + 5) && (10 - 2);
+console.log(condicional2);
+
+var corFavorita = 'Azul';
+switch (corFavorita) {
+    case 'Azul':
+        console.log('Olhe para o céu');
+        break;
+    case 'Amarelo':
+        console.log('Olhe para o sol');
+        break;
+    case 'Verde':
+        console.log('Olhe para a floresta');
+    default:
+        console.log('Feche os olhos');
+}
