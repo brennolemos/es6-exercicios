200a3c7c26f8a3dee0611a814a80df01cb3c83d4
Breno Lemos <brenomiros@gmail.com>
2019-02-08 23:04:44 -0200

    Exercícios da aula de Boolean e Condicionais

---
+++ js-para-iniciantes/04-booleans-e-condicionais/script.js
@@ -0,0 +1,41 @@
+// Verifique se a sua idade é maior do que a de algum parente
+// Dependendo do resultado coloque no console 'É maior', 'É igual' ou 'É menor'
+var minhaIdade = 24;
+var idadeRobert = 30;
+
+if (minhaIdade > idadeRobert) {
+    console.log('É maior');
+} else {
+    console.log('É menor');
+}
+
+// Qual valor é retornado na seguinte expressão?
+var expressao = (5 - 2) && (5 - ' ') && (5 - 2); //NaN
+console.log(expressao);
+
+// Verifique se as seguintes variáveis são Truthy ou Falsy
+var nome = 'Andre'; //Truthy
+var idade = 28; //Truthy
+var possuiDoutorado = false; //Falsy
+var empregoFuturo; //Falsy
+var dinheiroNaConta = 0; //Falsy
+
+// Compare o total de habitantes do Brasil com China (valor em milhões)
+var brasil = 207;
+var china = 1340;
+
+// O que irá aparecer no console?
+if(('Gato' === 'gato') && (5 > 2)) {
+  console.log('Verdadeiro');
+} else {
+  console.log('Falso');
+}
+// Falso
+
+// O que irá aparecer no console?
+if(('Gato' === 'gato') || (5 > 2)) {
+  console.log('Gato' && 'Cão');
+} else {
+  console.log('Falso');
+}
+//Cão
