23370f068d06e584600909881548b29e54b04443
Breno Lemos <brenomiros@gmail.com>
2019-02-10 13:09:56 -0200

    Script Funções 2

--- js-para-iniciantes/05-funcoes/script-aula.js
+++ js-para-iniciantes/05-funcoes/script-aula.js
@@ -30,4 +30,43 @@ function corFavorita(cor) {
 
 addEventListener('click', function () {
     console.log('Oi');
-})
+});
+
+function imc2(peso, altura) {
+    var imc = peso / (altura ** 2);
+    console.log(imc);
+}
+
+imc2(80, 1.8);
+
+function terceiraIdade(idade) {
+    console.log(typeof idade);
+    if (typeof idade !== 'number') {
+        return 'Por favor, preencha um número';
+    } else if (idade >= 60) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(terceiraIdade('oi'));
+
+function faltaVisitar(paisesVisitados) {
+    var totalPaises = 193;
+    return `Faltam visitar ${totalPaises - paisesVisitados} países`;
+}
+
+var profissao = 'Designer';
+function dados() {
+    var nome = 'André';
+    var idade = 28;
+    function outrosDados() {
+        var endereco = 'Rio de Janeiro';
+        var idade = 29;
+        return `${nome}, ${idade}, ${endereco}, ${profissao}`;
+    }
+    return outrosDados();
+}
+
+console.log(dados());
