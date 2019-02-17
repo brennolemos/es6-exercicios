0477546c0ea2c8f7c18aeb63bbcb58bdf2579f7f
Breno Lemos <brenomiros@gmail.com>
2019-02-17 16:14:50 -0300

    Script Objetos 2

--- js-para-iniciantes/06-objetos/script-aula.js
+++ js-para-iniciantes/06-objetos/script-aula.js
@@ -17,3 +17,19 @@ console.log(quadrado.area(5));
 console.log(quadrado.perimetro(5));
 
 console.log(Math.random());
+
+
+var menu = {
+    width: 800,
+    height: 50,
+    backgroundColor: '#84e',
+    metadeHeight() {
+        return this.height / 2;
+    }
+}
+menu.backgroundColor = "#000";
+menu.color = "blue";
+menu.esconder = function() {
+    console.log('Escondi');
+}
+var bg = menu.backgroundColor;
