ba4a2238cc6ab3efc612ff996b7a1749c689c6c1
Breno Lemos <brenomiros@gmail.com>
2019-02-17 16:54:25 -0300

    Exercícios de Tudo é Objeto

--- js-para-iniciantes/07-tudo-e-objeto/script.js
+++ js-para-iniciantes/07-tudo-e-objeto/script.js
@@ -1,6 +1,12 @@
 // nomeie 3 propriedades ou métodos de strings
+// String.bind();
+// String.length;
+// String.includes();
 
 // nomeie 5 propriedades ou métodos de elementos do DOM
+// document.addEventListener();
+// document.appendChild();
+
 
 // busque na web um objeto (método) capaz de interagir com o clipboard, 
 // clipboard é a parte do seu computador que lida com o CTRL + C e CTRL + V
