528f1c6e07f859893772a96f1e7fe0a4d193652a
Breno Lemos <brenomiros@gmail.com>
2019-03-19 10:05:57 -0300

    Script Arrays e Loops 1

---
+++ js-para-iniciantes/08-arrays-e-loops/script-aula.js
@@ -0,0 +1,15 @@
+var videoGames = ['Switch', 'PS4', 'XBOX'];
+
+var ultimoItem = videoGames.pop();
+
+videoGames.push('3DS');
+
+for (var numero = 0; numero < 10; numero++) {
+    console.log(numero)
+}
+
+var i = 0;
+while (i < 10) {
+    console.log(i);
+    i++;
+}
