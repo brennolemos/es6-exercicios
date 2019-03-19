81f4322924b030be3c0c7aee00af7d4463907a60
Breno Lemos <brenomiros@gmail.com>
2019-03-19 10:28:13 -0300

    Script Arrays e Loops 2

--- js-para-iniciantes/08-arrays-e-loops/script-aula.js
+++ js-para-iniciantes/08-arrays-e-loops/script-aula.js
@@ -1,6 +1,6 @@
 var videoGames = ['Switch', 'PS4', 'XBOX'];
 
-var ultimoItem = videoGames.pop();
+// var ultimoItem = videoGames.pop();
 
 videoGames.push('3DS');
 
@@ -12,4 +12,24 @@ var i = 0;
 while (i < 10) {
     console.log(i);
     i++;
+}
+
+for (var item = 0; item < videoGames.length; item++) {
+    console.log(videoGames[item]);
+    if (videoGames[item] === 'PS4') {
+        break;
+    }
+}
+
+var frutas = ['Banana', 'Pera', 'Uva', 'Maçã', 'Salada Mista'];
+
+frutas.forEach(function (item, index) {
+    console.log(item, index);
+});
+
+var numero = 0;
+var maximo = 15;
+for (; numero < maximo;) {
+    console.log(numero);
+    numero++;
 }
